3ef7eade821da707c4fda4b142fd7f78893ae3db
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-09 03:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ function prependDriver(name){
   const newDrivers = [name, ...drivers];
   return newDrivers;
 };
+
+function removeLastDriver(){
+  const newDrivers = drivers.slice(0, drivers.length-1)
+  return newDrivers;
+};
